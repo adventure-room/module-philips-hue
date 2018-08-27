6afc748d5a01a05c204df5a00817238ff71ce4ed
Erik Mulder <ewjmulder@yahoo.com>
2018-08-27 23:13:39 +0200

    Work in progress: regex action refactoring

--- dsl/src/main/java/com/programyourhome/adventureroom/module/philipshue/dsl/converters/AbstractPhilipsHueActionConverter.java
+++ dsl/src/main/java/com/programyourhome/adventureroom/module/philipshue/dsl/converters/AbstractPhilipsHueActionConverter.java
@@ -7,6 +7,8 @@ import com.programyourhome.adventureroom.model.script.action.Action;
 public abstract class AbstractPhilipsHueActionConverter<A extends Action> implements RegexActionConverter<A> {
 
     public static final RegexVariable LIGHT_ID = RegexActionConverter.RESOURCE_ID.withName("lightId");
+    public static final RegexVariable LIGHT_IDS = RegexActionConverter.RESOURCE_IDS.withName("lightIds");
     public static final RegexVariable COLOR_ID = RegexActionConverter.RESOURCE_ID.withName("colorId");
+    public static final RegexVariable COLOR_IDS = RegexActionConverter.RESOURCE_IDS.withName("colorIds");
 
 }
--- dsl/src/main/java/com/programyourhome/adventureroom/module/philipshue/dsl/converters/DimLightsActionConverter.java
+++ dsl/src/main/java/com/programyourhome/adventureroom/module/philipshue/dsl/converters/DimLightsActionConverter.java
@@ -1,5 +1,7 @@
 package com.programyourhome.adventureroom.module.philipshue.dsl.converters;
 
+import java.util.Map;
+
 import com.programyourhome.adventureroom.dsl.regex.MatchResult;
 import com.programyourhome.adventureroom.model.Adventure;
 import com.programyourhome.adventureroom.module.philipshue.model.DimLightsAction;
@@ -8,7 +10,15 @@ import com.programyourhome.adventureroom.module.philipshue.model.resources.light
 public class DimLightsActionConverter extends AbstractPhilipsHueActionConverter<DimLightsAction> {
 
     @Override
-    public String getRegexLine() {
+    public Map<String, String> getRegexMap() {
+        return this.createRegexes(
+                "single", "dim light " + LIGHT_ID + " to " + INTEGER,
+                "multiple", "dim lights " + RESOURCE_ID_LIST
+                "multiple", "dim all lights to " + INTEGER);
+    }
+
+    @Override
+    public String get() {
         return "dim the lights to " + INTEGER;
     }
 
